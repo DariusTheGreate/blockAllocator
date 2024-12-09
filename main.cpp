374481ca0fb085bfad48015df3a9428247999df5
DariusTheGreate <kotovshikov31415926@gmail.com>
2024-12-09 15:24:34 +0300

    added allocate deallocate

--- main.cpp
+++ main.cpp
@@ -26,29 +26,59 @@ class BlockAllocator
 
 		void* allocate()
 		{
-			return _chunksHead->next;
+			std::unique_lock<std::mutex>{_lock};
+			if(_blockSize + _usedMem > _blockSize * _blockCount)
+				return nullptr;
+
+			auto* curr = _chunksHead;
+			auto* next = _chunksHead->next;
+			_chunksHead = next;
+			_usedMem += _blockSize;
+
+			return curr;
 		}
 
-		void deallocate()
+		void* allocate(uint32_t sz_in)// Think about it
 		{
+			std::unique_lock<std::mutex>{_lock};
+			if(sz_in % _blockSize != 0)	
+				throw std::runtime_error("BlockAllocator::allocate(uint32_t): Trying to allocate size not aligned with blockSize");
+			return nullptr;
+		}
 
+		void deallocate(void* ptr)
+		{
+			std::unique_lock<std::mutex>{_lock};
+			auto* newChunk = reinterpret_cast<Chunk*>(ptr); // NULLIFY HERE?
+		    newChunk->next = _chunksHead;
+		    _chunksHead = newChunk;
+		    _usedMem -= _blockSize;
+		}
+
+		~BlockAllocator()
+		{
+			free(_dataPtr);	
 		}
 
 	private:
 		uint32_t   _blockSize  = 0;
 		uint32_t   _blockCount = 0;
-		uint32_t   _currBlock  = 0;
+		uint32_t   _usedMem    = 0;
 		void*      _dataPtr    = nullptr;
 		std::mutex _lock;
 
 		 // Stack linked list for free chunks
 	    struct Chunk { Chunk* next; };
-	    Chunk* _chunksHead = nullptr;
+	    Chunk*     _chunksHead = nullptr;
 };
 
 int main()
 {
 	BlockAllocator alloc{1024, 1024};
+
+	void* dat = alloc.allocate();
+
+	alloc.deallocate(dat);
 
 	return 0;
 }
